58f84574285959f6be7116ef308bb08d411f6a11
phuong-tran-jh <phuong.tran@journeyh.io>
2025-06-15 17:37:08 +0700

    chore: add score and close the modal via ESC

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -8,14 +8,17 @@ export default function ResultModal({
   const dialog = useRef();
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
   useImperativeHandle(ref, () => ({
     open() {
       dialog.current.showModal();
     },
   }));
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+
       <p>
         The target time was <strong>{targetTime} seconds</strong>
       </p>
